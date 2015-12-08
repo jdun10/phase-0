134d02d069a20509646edf193450b2ec82ed04a6
jdun10 <jilliandunleavy7779@gmail.com>
2015-12-08 04:31:06 -0500

    Create concatenate-arrays-my-solution.rb

---
+++ week-4/concatenate-arrays-my-solution.rb
@@ -0,0 +1,15 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def concat_string
+  arr1 = ["red", "orange", "yellow"]
+  arr2 = ["green", "blue", "indigo", "violet"]
+  arr3 = arr1 + arr2
+  puts arr3
+end
+
+concat_string
