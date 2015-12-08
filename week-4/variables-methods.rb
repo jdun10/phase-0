b9f8823e9ddd236f7ccec4cdabe80f04067180ab
jdun10 <jilliandunleavy7779@gmail.com>
2015-12-07 20:17:37 -0500

    Create variables-methods.rb

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,24 @@
+# I worked on this challenge by myself
+
+def do_something
+	my_string = "well well well"
+	puts my_string
+	puts my_string.reverse
+	puts my_string.upcase
+end
+
+# https://github.com/jdun10/phase-0/blob/master/week-4/address-my_solution.rb
+# https://github.com/jdun10/phase-0/blob/master/week-4/math_my_solution.rb
+
+# How do you define a local variable?
+#	  You set it equal to its placeholder within the method with a = sign.
+# How do you define a method?
+#   You define a method by typing "def method_name", then setting the parameters of the code, and finishing with "end".
+# What is the difference between a local variable and a method?
+#   A local variable is a set piece of data that exists only within the method. A method is the execution of all of the contained code.
+# How do you run a ruby program from the command line?
+#   By typing irb and then putting the whole program in the terminal.
+# How do you run an RSpec file from the command line?
+#   rspec file_name.rb
+# What was confusing about this material? What made sense?
+#   Sometimes I would forget about how I need to add spaces inbetween strings myself rather than have them come out all as one word.
