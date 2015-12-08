b8694e057e2d5a39047901b36685c3afed233411
jdun10 <jilliandunleavy7779@gmail.com>
2015-12-07 20:08:23 -0500

    Create math_my_solution.rb

---
+++ week-4/math_my_solution.rb
@@ -0,0 +1,17 @@
+# I worked on this challenge by myself
+
+def math(num1, num2)
+  add = num1 + num2
+  puts add
+  
+  subtract = num1 - num2
+  puts subtract
+  
+  multiply = num1 * num2
+  puts multiply
+  
+  divide = num1 / num2.to_f
+  puts divide
+end
+
+math(4, 2)
