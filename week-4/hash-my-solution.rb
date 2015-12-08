d4273314ee96c7b22cc69e6aa3a5a59cf22aa7eb
jdun10 <jilliandunleavy7779@gmail.com>
2015-12-08 05:33:05 -0500

    Create hash-my-solution.rb

---
+++ week-4/hash-my-solution.rb
@@ -0,0 +1,12 @@
+# Define a Hash
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+# my_info is empty to start.  Edit this!
+my_info = {}
+
+my_info = { "first_name" => "Jill", "last_name" => "Dunleavy", "hometown" => "Sayville", "age" => 26 }
+puts my_info["age"]
