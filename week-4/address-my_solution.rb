79b2b47e46991fa947cc8ffd1608951eaceef88b
jdun10 <jilliandunleavy7779@gmail.com>
2015-12-07 19:58:01 -0500

    Create address-my_solution.rb

---
+++ week-4/address-my_solution.rb
@@ -0,0 +1,11 @@
+# Format an Address
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+def make_address(street, city, state, zip)
+  puts "You live at " + street + ' ' + city + ' ' + state + ' ' + zip + '.'
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
