63554ec97218e1b8083fe4a57bc9d4bbf8972401
jdun10 <jilliandunleavy7779@gmail.com>
2015-12-08 00:41:18 -0500

    Create simple-substrings-my-solution.rb

---
+++ week-4/simple-substrings-my-solution.rb
@@ -0,0 +1,9 @@
+# I worked on this challenge by myself
+
+def welcome(address)
+  if address.include?("CA")
+    return "Welcome to California"
+  else
+    return "You should move to California"
+  end
+end
