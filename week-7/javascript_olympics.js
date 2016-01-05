6ea37f5dbe0cc6eca400dd6d29550674f37e565e
jdun10 <jilliandunleavy7779@gmail.com>
2016-01-04 20:50:02 -0500

    Update javascript_olympics.js

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -73,5 +73,13 @@ console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps
 
 // Reflection
 // What JavaScript knowledge did you solidify in this challenge?
+//	I really liked having this challenge, it was helpful to do things that we had already done in Ruby but get settled into using JS instead.
+//	Having something that wasn't too difficult in terms of logic but that forced us to get more comfortable in JS was great.
+
 // What are constructor functions?
+//	Constructor functions seem kind of attributes in a Ruby class. You could have different names, ages, sports, and quotes for different people if we
+//	expanded the program, but we only used one in this example. It sets different attributes of the same name to a broader group.
+
 // How are constructors different from Ruby classes (in your research)?
+//	I think that constructors only work locally, but maybe that's just how we used them. The constructor "this" made sure that the name, age, sport,
+//	and quote all went to the right athlete in this function.
